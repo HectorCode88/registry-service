355aad767c5fa38e651dc7c630d8a39cc8e5b644
Hector Ccasani <hccasani.hc@gmail.com>
2020-02-05 22:49:00 -0500

    created registry-service using the Netflix Eureka

---
+++ src/test/java/pe/ccasani/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.ccasani.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
